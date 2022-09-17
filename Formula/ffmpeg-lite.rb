cb26ff8f6f93cf601ca30e6e4c40d3550119cadf
Zachary Waldowski <zach@waldowski.me>
2022-09-17 11:12:37 -0400

    ffmpeg-lite: v5.1.1

--- Formula/ffmpeg-lite.rb
+++ Formula/ffmpeg-lite.rb
@@ -1,8 +1,8 @@
 class FfmpegLite < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.0.tar.xz"
-  sha256 "51e919f7d205062c0fd4fae6243a84850391115104ccf1efc451733bc0ac7298"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.1.tar.xz"
+  sha256 "95bf3ff8c496511e71e958fb249e663c8c9c3de583c5bebc0f5a9745abbc0435"
   license "GPL-2.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
@@ -18,13 +18,13 @@ class FfmpegLite < Formula
 
   depends_on "nasm" => :build
   depends_on "pkg-config" => :build
-
   depends_on "lame"
   depends_on "x264"
   depends_on "x265"
   depends_on "xz"
 
   uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
   conflicts_with "ffmpeg", because: "both install `ffmpeg` binaries"
@@ -46,6 +46,7 @@ class FfmpegLite < Formula
     ]
 
     # Needs corefoundation, coremedia, corevideo
+    args << "--enable-videotoolbox" if OS.mac?
     args << "--enable-neon" if Hardware::CPU.arm?
 
     system "./configure", *args
